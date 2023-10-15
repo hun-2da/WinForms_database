1976208d0c1af16c91ebb8090511f97b8f58dd16
20gns <b_h14@naver.ccom>
2023-10-15 11:03:59 +0900

    데이터베이스 mysql 세팅

--- WinForms_database/Form1.cs
+++ WinForms_database/Form1.cs
@@ -1,3 +1,4 @@
+// Scaffold-DbContext "server=localhost;database=univDB;uid=root;pwd=201104" MySql.EntityFrameworkCore -OutputDir univDB -f
 namespace WinForms_database
 {
     public partial class Form1 : Form
@@ -5,6 +6,11 @@ namespace WinForms_database
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
