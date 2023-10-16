1190c7abc5e590a55637ab5fe439ccbb8bd3d2bf
20gns <b_h14@naver.ccom>
2023-10-16 16:09:39 +0900

    asd

---
+++ WinForms_database/univDB/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WinForms_database.univDB;
+
+public partial class Student
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string? Address { get; set; }
+
+    public int Grade { get; set; }
+
+    public int? Age { get; set; }
+
+    public string Gender { get; set; } = null!;
+
+    public string? Phone { get; set; }
+
+    public string Dept { get; set; } = null!;
+
+    public virtual ICollection<Class> Classes { get; set; } = new List<Class>();
+}
